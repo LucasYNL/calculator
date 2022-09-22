5054e89f8d97e06e5a922a9ec6d84c8ebc1611bf
LucasYNL <michaelnggg@hotmail.com>
2022-09-21 20:13:35 -0700

    javascript for basic math

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return x - y;
+}
+
+function multiply(x, y){
+    return x * y;
+}
+
+function divide(x, y){
+    return x / y;
+}
+
+let a = Number(prompt("Enter a number to add."));
+let b = Number(prompt('Enter another one.'));
+
+alert(divide(a, b));
