c3f091fd4397ec928c50b16f6e46db6953ce0fef
LucasYNL <michaelnggg@hotmail.com>
2022-09-24 11:51:21 -0700

    numbers and operators now will display on screen upon click, clear btn working

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+const numbers = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.ops');
+const clear = document.querySelector('.clear');
+const display = document.querySelector('.display');
+let valueOne = '';
+let valueTwo = '';
+let oper = '';
+let screenDisplay = '';
+
+numbers.forEach(function (x){
+    x.addEventListener('click', () =>{
+        if(oper == ''){
+            display.value = '';
+            valueOne += x.value;
+            screenDisplay += x.value;
+            display.value = screenDisplay;
+        }else if(oper != ''){
+            valueTwo += x.value;
+            screenDisplay += x.value;
+            display.value = screenDisplay;
+        }
+    });
+});
+
+operators.forEach(function(y){
+    y.addEventListener('click', () =>{
+        oper = y.value;
+        screenDisplay += y.value;
+        display.value = screenDisplay;
+    });
+});
+
+clear.addEventListener('click', () =>{
+    valueOne, valueTwo, oper, screenDisplay = '';
+    display.value = '0';
+});
+
 function add(x, y){
     return x + y;
 }
@@ -19,10 +56,10 @@ function operate(num1, num2, op){
         add(num1, num2);
     }else if(op == '-'){
         subtract(num1, num2);
-    }else if(op =='*'){
+    }else if(op =='&times'){
         multiply(num1, num2);
-    }else if(op =='/'){
+    }else if(op =='&#247'){
         divide(num1, num2);
     }
-    
+
 }
