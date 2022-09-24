b90bf509d8aaccc777ca4cf8fbadd8158e58d91d
LucasYNL <michaelnggg@hotmail.com>
2022-09-23 19:47:53 -0700

    Created calculator ui

--- script.js
+++ script.js
@@ -14,7 +14,15 @@ function divide(x, y){
     return x / y;
 }
 
-let a = Number(prompt("Enter a number to add."));
-let b = Number(prompt('Enter another one.'));
-
-alert(divide(a, b));
+function operate(num1, num2, op){
+    if(op == '+'){
+        add(num1, num2);
+    }else if(op == '-'){
+        subtract(num1, num2);
+    }else if(op =='*'){
+        multiply(num1, num2);
+    }else if(op =='/'){
+        divide(num1, num2);
+    }
+    
+}
