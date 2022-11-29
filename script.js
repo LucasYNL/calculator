ec75bf0e70df1b0426c84d6cd2e7d18ac0567500
LucasYNL <michaelnggg@hotmail.com>
2022-11-29 15:44:52 -0800

    theory update, later half . not working yet

--- script.js
+++ script.js
@@ -4,15 +4,9 @@ const dot = document.querySelector('#dot');
 
 let stor = [];
 let runnable = true;
-let finished = false;
 
 eles.forEach(function (x){
     x.addEventListener('click', () =>{
-        if(finished === true){
-            clearDisplay();
-            finished = false;
-        }
-
         if(stor.length <= 9){
             if(x.name != ""){
                 stor.push(x.name);
@@ -21,44 +15,29 @@ eles.forEach(function (x){
             }else{
                 stor.push(x.value);
                 display.value = stor.join('');
-                eles.forEach(ops => ops.disabled = false);
             }
         }
 
         if(x.value === '.'){
-            eles.forEach(ops => ops.disabled = false);
             dot.disabled = true;
         }
     });
 });
 
 function clearDisplay(){
-    let temp = [];
-    stor = temp;
+    reset();
     display.value = '0';
     dot.disabled = false;
-    eles.forEach(ops => ops.disabled = false);
 }
 
-function restart(){
-    alert('Please enter a proper calculation');
-    clearDisplay();
+function wrong(){
+    display.value = 'ERROR';
+    reset();
 }
 
-function add(x, y){
-    return x + y;
-}
-
-function subtract(x, y){
-    return x - y;
-}
-
-function multiply(x, y){
-    return x * y;
-}
-
-function divide(x, y){
-    return x / y;
+function reset(){
+    let temp = [];
+    stor = temp;
 }
 
 function calculate(x, y, op){
@@ -67,16 +46,16 @@ function calculate(x, y, op){
     y = Number(y);
 
     if(stor[op] === '+'){
-        total = add(x, y);
+        total = x + y;
     }else if(stor[op] === '-'){
-        total = subtract(x, y);
+        total = x - y;
     }else if(stor[op] == '×'){
-        total = multiply(x, y);
+        total = x * y;
     }else if(stor[op] == '÷'){
-        total = divide(x, y);
+        total = x / y;
     }else{
         runnable = false;
-        restart();
+        wrong();
     }
 
     total = total.toString();
@@ -96,26 +75,25 @@ function operate(){
     let sum = '';
     let temp = [];
 
-    if(isNaN(Number(stor[0])) === true || isNaN(Number(stor[size - 1])) === true){
+    if(isNaN(Number(stor[0])) === true && isNaN(Number(stor[0])) === '.' || isNaN(Number(stor[size - 1])) === true){
         runnable = false;
-        restart();
+        console.log('check 1');
+        wrong();
     }
 
     if(runnable === true){
+        console.log('check 2');
         let op = 0;
         for(let i = 0; i < size; i++){
             if(isNaN(Number(stor[i])) === false || stor[i] === '.'){ //finds the beginning sum
                 sum += stor[i];
-                console.log('1st num');
             }else if(isNaN(Number(stor[i])) === true && isNaN(Number(stor[i + 1])) === false){ //finds operator and keep track of it and finds the sum to be calculated with.
                 op = i;
                 let sum2 = '';
                 for(let x = i + 1; x <= size; x++){ //finds the sum of second.
                     if(isNaN(Number(stor[x])) === false || stor[x] === '.'){ //finds the beginning sum
                         sum2 += stor[x];
-                        console.log('1st num');
                     }else if(isNaN(Number(stor[x])) === true || x === size){ //finds end location and update index
-                        console.log('found step')
                         i = x - 1;
                         break;
                     }
@@ -124,7 +102,7 @@ function operate(){
                 sum = calculate(sum, sum2, op);            
             }else{
                 runnable = false;
-                restart();
+                wrong();
                 break;
             }
         }
@@ -135,7 +113,6 @@ function operate(){
 
     if(runnable === true){
         display.value = sum;
-        finished = true;
     }else if(runnable === false){
         runnable = true;
     }
